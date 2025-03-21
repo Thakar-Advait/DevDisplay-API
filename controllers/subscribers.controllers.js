84c17891329fefd44db21c89dfe3b007d3d752fb
Thakar-Advait <advaitthakar18@gmail.com>
2025-03-22 03:24:39 +0530

    some changes

--- controllers/subscribers.controllers.js
+++ controllers/subscribers.controllers.js
@@ -2,9 +2,7 @@ import Subscribers from "../models/subscribers.models.js";
 
 export const getSubscribers = async (req, res) => {
     try {
-        const subscribers = await Subscribers.find({
-            $where: "this.status === 'subscribed'"
-        });
+        const subscribers = await Subscribers.find().where("status").equals("subscribed");
         res.status(200).json(subscribers);
     } catch (error) {
         res.status(500).json({ message: error.message });
